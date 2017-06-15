7ed9efbc5cfc01ed6d823f52a9e306470b48553c
Lee JeYeol <egg5562@naver.com>
2017-06-15 13:25:47 +0900

    InfoGAN Implementation prototype

--- GAN/InfoGAN.py
+++ GAN/InfoGAN.py
@@ -54,6 +54,7 @@ parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
 parser.add_argument('--netG', default='', help="path of Generator networks.(to continue training)")
 parser.add_argument('--netD', default='', help="path of Discriminator networks.(to continue training)")
+parser.add_argument('--netQ', default='', help="path of Auxiliaty distribution networks.(to continue training)")
 parser.add_argument('--outf', default='.', help="folder to output images and model checkpoints")
 parser.add_argument('--seed', type=int, help='manual seed')
 
@@ -76,6 +77,7 @@ torch.manual_seed(options.seed)
 if options.cuda:
     torch.cuda.manual_seed(options.seed)
 
+torch.backends.cudnn.benchmark = True
 cudnn.benchmark = True
 if torch.cuda.is_available() and not options.cuda:
     print("WARNING: You have a CUDA device, so you should probably run with --cuda")
@@ -106,11 +108,11 @@ ncatC = 10
 # Models
 #======================================================================================================================
 
-# todo add 4 datasets and models
+
 # todo netG add c
 # todo netD add Q()
 # todo result visualize
-
+# todo add 4 datasets and models
 
 
 # custom weights initialization called on netG and netD
@@ -122,6 +124,9 @@ def weights_init(m):
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
 
+
+
+# Generator ============================================================================================================
 class _netG(nn.Module):
     def __init__(self, ngpu):
         super(_netG, self).__init__()
@@ -159,12 +164,16 @@ if options.netG != '':
     netG.load_state_dict(torch.load(options.netG))
 print(netG)
 
-
+# Discriminator ========================================================================================================
 class _netD(nn.Module):
     def __init__(self, ngpu):
         super(_netD, self).__init__()
         self.ngpu = ngpu
-
+        self.main_netD = nn.Sequential(
+            # 1024 => 1
+            nn.Conv2d(1024, 1, 1, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
         self.main = nn.Sequential(
             # 1*28*28 => 64*14*14
             nn.Conv2d(in_channels=1, out_channels=64, kernel_size=4, stride=2, padding=1),
@@ -178,25 +187,62 @@ class _netD(nn.Module):
             # 128*7*7 => 1024
             nn.Conv2d(128, 1024, 7, 1, 0, bias=False),
             nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            # 1024 => 1
-            nn.Conv2d(1024, 1, 1, 1, 0, bias=False),
-            nn.Sigmoid()
+            nn.LeakyReLU(0.2, inplace=True)
         )
 
     def forward(self, input):
         if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+            outputD = nn.parallel.data_parallel(self.main_netD, output, range(self.ngpu))
         else:
             output = self.main(input)
-        return output
+            outputD = self.main_netD(output)
+        return outputD, output
 
 netD = _netD(ngpu)
 netD.apply(weights_init)
 if options.netD != '':
-    netG.load_state_dict(torch.load(options.netD))
+    netD.load_state_dict(torch.load(options.netD))
 print(netD)
+
+# Auxiliary distribution ===============================================================================================
+class _netQ(nn.Module):
+    def __init__(self, ngpu):
+        super(_netQ, self).__init__()
+        self.ngpu = ngpu
+        self.main_netQ = nn.Sequential(
+            # 1024 => 128
+            nn.Conv2d(1024, 128, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.cat_netQ = nn.Sequential(
+            # 128 -> onehot code
+            nn.Conv2d(128, ncatC, 1, 1, 0, bias=False),
+            nn.Softmax2d()
+        )
+        self.con_netQ = nn.Sequential(
+            # 128 ->
+            nn.Conv2d(128, nconC, 1, 1, 0, bias=False),
+        )
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            outputQ = nn.parallel.data_parallel(self.main_netQ, input, range(self.ngpu))
+            c_cont = nn.parallel.data_parallel(self.con_netQ, outputQ, range(self.ngpu))
+            c_cat = nn.parallel.data_parallel(self.cat_netQ, outputQ, range(self.ngpu))
+        else:
+            outputQ = self.main_netQ(input)
+            c_cont = self.con_netQ(outputQ)
+            c_cat = self.cat_netQ(outputQ)
+        return c_cat, c_cont
+
+netQ = _netQ(ngpu)
+netQ.apply(weights_init)
+if options.netQ != '':
+    netQ.load_state_dict(torch.load(options.netQ))
+print(netQ)
+
 
 
 #======================================================================================================================
@@ -224,6 +270,7 @@ fake_label = 0
 if options.cuda:
     netD.cuda()
     netG.cuda()
+    netQ.cuda()
     criterion.cuda()
     input, label = input.cuda(), label.cuda()
     final_noise, noise, fixed_noise, noise_c1, noise_c2, onehot_c = final_noise.cuda(), noise.cuda(), fixed_noise.cuda(), noise_c1.cuda(),\
@@ -245,25 +292,27 @@ fixed_noise = Variable(fixed_noise)
 # setup optimizer
 optimizerD = optim.Adam(netD.parameters(), lr=options.lr, betas=(options.beta1, 0.999))
 optimizerG = optim.Adam(netG.parameters(), lr=options.lr, betas=(options.beta1, 0.999))
+optimizerQ = optim.Adam(netQ.parameters(), lr=options.lr, betas=(options.beta1, 0.999))
+
 
 for epoch in range(options.iteration):
     for i, data in enumerate(dataloader, 0):
         ############################
         # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
         ###########################
-        # train with real
+        # train with real data
         netD.zero_grad()
         real_cpu, _ = data
         batch_size = real_cpu.size(0)
         input.data.resize_(real_cpu.size()).copy_(real_cpu)
         label.data.resize_(batch_size).fill_(real_label)
 
-        output = netD(input)
-        errD_real = criterion(output, label)
+        outputD, midQ = netD(input)
+        errD_real = criterion(outputD, label)
         errD_real.backward()
-        D_x = output.data.mean()
+        D_x = outputD.data.mean()
 
-        # train with fake
+        # generate noise
         noise.data.resize_(batch_size, nz, 1, 1)
         noise.data.normal_(0, 1)
         noise_c1.data.resize_(batch_size, 1, 1, 1)
@@ -271,30 +320,38 @@ for epoch in range(options.iteration):
         noise_c2.data.resize_(batch_size, 1, 1, 1)
         noise_c2.data.normal_(0, 1)
 
-        onehot_c.data = one_hot((batch_size, 10), torch.LongTensor([random.randrange(0, 10) for i in range(64)]).view(-1, 1)).cuda()
+        onehot_c.data = one_hot((batch_size, 10), torch.LongTensor([random.randrange(0, 10) for i in range(batch_size)]).view(-1, 1)).cuda()
         onehot_c = onehot_c.float()
         onehot_c.data.resize_(batch_size, 10, 1, 1)
 
         final_noise = torch.cat((noise, noise_c1, noise_c2, onehot_c), 1)
 
-        fake = netG(final_noise )
+        #train with fake data
+        fake = netG(final_noise)
         label.data.fill_(fake_label)
-        output = netD(fake.detach())
-        errD_fake = criterion(output, label)
-        errD_fake.backward()
-        D_G_z1 = output.data.mean()
-        errD = errD_real + errD_fake
-        optimizerD.step()
 
+        outputD, midQ = netD(fake.detach())
+        c_cat, c_cont = netQ(midQ.detach())
+
+        errD_fake = criterion(outputD, label)
+        errD_fake.backward()
+        errQ_fake = criterion(torch.cat((c_cat, c_cont), 1), torch.cat((onehot_c, noise_c1, noise_c2), 1))
+        errQ_fake.backward()
+
+        D_G_z1 = outputD.data.mean()
+        errD = errD_real + errD_fake
+
+        optimizerD.step()
+        optimizerQ.step()
         ############################
         # (2) Update G network: maximize log(D(G(z)))
         ###########################
         netG.zero_grad()
         label.data.fill_(real_label)  # fake labels are real for generator cost
-        output = netD(fake)
-        errG = criterion(output, label)
+        outputD, midQ = netD(fake)
+        errG = criterion(outputD, label)
         errG.backward()
-        D_G_z2 = output.data.mean()
+        D_G_z2 = outputD.data.mean()
         optimizerG.step()
 
         print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
@@ -312,3 +369,7 @@ for epoch in range(options.iteration):
     # do checkpointing
     torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (options.outf, epoch))
     torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (options.outf, epoch))
+
+# Je Yeol Lee
+# \[T]/
+# Jolly Co-operation
