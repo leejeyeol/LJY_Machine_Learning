d2066928ae830bbc876a1dd908a750465b8b37c0
leejeyeol <leejeyeol92@gmail.com>
2017-10-27 02:20:35 +0900

    save

--- Automatic_Polyp_Detection/7_STD_train.py
+++ Automatic_Polyp_Detection/7_STD_train.py
@@ -3,4 +3,21 @@
 # selected superpixels are listed to samples.
 # all samples's features are used to MKBoosting.
 # train MKB. and make strong classifier
-training_set = [data, label]
+from sklearn.svm import SVR
+
+training_set = [['each feature'], ['label']]
+feature_list = ['color_hist', 'LM', 'LBP', 'HOG']
+num_of_features = 4
+kernel_list = ['linear', 'rbf', 'sigmoid']
+num_of_kernels = 3
+iteration = 100
+
+
+# weights [features][kernels]
+weights = [[[1/num_of_features],[1/num_of_features],[1/num_of_features]],[[1/num_of_features],[1/num_of_features],[1/num_of_features]],[[1/num_of_features],[1/num_of_features],[1/num_of_features]],[[1/num_of_features],[1/num_of_features],[1/num_of_features]]]
+
+for feature in feature_list:
+    for kernel in kernel_list:
+        clf = SVR(kernel=kernel)
+        clf.fit(training_set[0], training_set[1])
+        clf.predict(test_set[0])
