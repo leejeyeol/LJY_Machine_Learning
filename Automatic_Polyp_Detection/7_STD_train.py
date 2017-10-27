971a15cbd1e1ac40bbac57b5ce39c8c5343f9b9b
leejeyeol <leejeyeol92@gmail.com>
2017-10-27 15:36:03 +0900

    save

--- Automatic_Polyp_Detection/7_STD_train.py
+++ Automatic_Polyp_Detection/7_STD_train.py
@@ -4,6 +4,7 @@
 # all samples's features are used to MKBoosting.
 # train MKB. and make strong classifier
 from sklearn.svm import SVR
+import numpy as np
 
 training_set = [['each feature'], ['label']]
 feature_list = ['color_hist', 'LM', 'LBP', 'HOG']
@@ -11,13 +12,36 @@ num_of_features = 4
 kernel_list = ['linear', 'rbf', 'sigmoid']
 num_of_kernels = 3
 iteration = 100
+F = 0
+
+def MKBerror(result_vector, label_vector, weights):
+    threshold = (1.5/len(result_vector)*모든 샘플에 대해 각 샘플마다 (label - result_vector)의 l2 norm)
+    error = 모든 샘플에 대해 각 샘플마다 weights * sign(abs(result - label)-threshold)
+    return error
 
 
 # weights [features][kernels]
 weights = [[[1/num_of_features],[1/num_of_features],[1/num_of_features]],[[1/num_of_features],[1/num_of_features],[1/num_of_features]],[[1/num_of_features],[1/num_of_features],[1/num_of_features]],[[1/num_of_features],[1/num_of_features],[1/num_of_features]]]
-
+SVMs = [[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]]]
 for feature in feature_list:
     for kernel in kernel_list:
         clf = SVR(kernel=kernel)
         clf.fit(training_set[0], training_set[1])
-        clf.predict(test_set[0])
+        SVMs[feature][kernel] = clf
+
+min_error = 100
+min_error_index = [0,0]
+for i in range(0, iteration):
+    for feature in feature_list:
+        for kernel in kernel_list:
+            error = MKBerror(SVMs[feature][kernel].predict(testdata),label,weights[feature][kernel])
+            if error < min_error :
+                min_error = error
+                min_error_index = [feature,kernel]
+    if min_error < 0.5:
+        F = F + (1/2) * np.log((1-min_error)/min_error)*SVMs[min_error_index[0],min_error_index[1]]
+    else:
+        break
+
+
+
