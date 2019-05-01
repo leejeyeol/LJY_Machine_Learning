98019cf15994c9724a88ffc4b91a7fd2ef15ea68
DESKTOP-RH6PUJO\rnt <egg5562@naver.com>
2019-05-01 19:09:20 +0900

    test3

--- LJY_visualize_tools.py
+++ LJY_visualize_tools.py
@@ -54,7 +54,11 @@ def draw_lines_to_windict(win_dict, value_list, legend_list, epoch, iteration, t
     # epoch * total_iter + iteration
 
     num_of_values = len(value_list)
+    print(type(value_list))
+    print(type(value_list[0]))
+
     for (i, value_tensor) in  enumerate(value_list):
+
         if value_tensor.is_cuda :
             value_tensor = value_tensor.cpu()
         np.asarray(value_tensor)
