62349bbed060957ef116885d2b90b642c8a6ce2b
DESKTOP-RH6PUJO\rnt <egg5562@naver.com>
2019-05-08 17:13:18 +0900

    exp test 7

--- Pytorch/experiment.sh
+++ Pytorch/experiment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 for i in {0..225}
 do
-   python3 /home/mlpa/data_ssd/workspace/github/LJY_Machine_Learning/Pytorch/GAN/VAE+GAN.py --preset alpha-gan --cuda --dataset CelebA --runfunc GAM --pretrainedEpoch $i
+   python3 /home/mlpa/data_ssd/workspace/github/LJY_Machine_Learning/Pytorch/GAN/VAE+GAN.py --preset alpha-gan --cuda --dataset CelebA --batchSize 384 --runfunc GAM --pretrainedEpoch $i
 done
 
 for i in {0..253}
 do
-   python3 /home/mlpa/data_ssd/workspace/github/LJY_Machine_Learning/Pytorch/GAN/VAE+GAN.py --preset ours --cuda --dataset CelebA --runfunc GAM --pretrainedEpoch $i
+   python3 /home/mlpa/data_ssd/workspace/github/LJY_Machine_Learning/Pytorch/GAN/VAE+GAN.py --preset ours --cuda --dataset CelebA --batchSize 384 --runfunc GAM --pretrainedEpoch $i
 done
 
 
