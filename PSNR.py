9123676744b822215d4ebbddc4571a97022dab86
DESKTOP-RH6PUJO\rnt <egg5562@naver.com>
2019-05-18 00:52:12 +0900

    exp2 test2

--- PSNR.py
+++ PSNR.py
@@ -1,6 +1,8 @@
 import numpy
 import math
 import cv2
+import argparse
+
 import glob
 import os
 import numpy as np
@@ -15,17 +17,38 @@ def psnr(img1, img2):
 
 #d=psnr(original,contrast)
 
+parser = argparse.ArgumentParser()
+# Options for path =====================================================================================================
+parser.add_argument('--preset', default='None', help='', choices=['None','ours','dcgan','alpha-gan'])
+
+options = parser.parse_args()
 
 
 if __name__ == '__main__':
-  cases=  ['alpha-gan','alpha-gan','alpha-gan','alpha-gan','alpha-gan','ours','ours','ours','ours','ours']
-  eps = [22,28,67,98,122,22,28,67,98,122]
-  for i in range(len(cases)):
+    preset = 'alpha-gan'
+
+    #eps = [22,28,67,98,122,22,28,67,98,122]
+
+    alpha_ep = [4, 5, 8, 10, 13, 15, 16, 19, 21, 23, 24, 25, 28, 29, 30, 31, 32, 33, 34, 35, 36, 39, 40, 41, 43, 44, 51,
+              54, 55, 57, 59, 61, 66, 69, 72, 74, 75, 76, 78, 79, 82, 83, 85, 86, 87, 90, 92, 93, 94, 97, 99, 103, 104,
+              105, 106, 107, 109, 110, 111, 112, 113, 118, 119, 120, 122, 131]
+
+    ours_ep = [84, 13, 74, 57, 75, 57, 21, 54, 11, 16, 57, 106, 100, 97, 47, 57, 63, 47, 37, 123, 24, 57, 123, 28, 78, 64,
+             93, 100, 73, 75, 40, 73, 23, 35, 34, 54, 123, 35, 24, 14, 24, 73, 43, 41, 77, 106, 118, 21, 106, 49, 47,
+             43, 63, 22, 118, 123, 74, 21, 23, 13, 63, 22, 17, 84, 63, 15]
+
+    cases = [preset for _ in range(len(preset))]
+
+    for i in range(len(cases)):
       supplementNaN = True
       case = cases[i]
-      ep = eps[i]
+        if preset == 'alpha-gan':
+            ep = alpha_ep[i]
+        elif preset == 'ours':
+            ep = ours_ep[i]
 
-      img_folder = os.path.join('/home/mlpa/Workspace/github/LJY_Machine_Learning/Pytorch/results', case+'_' +str(ep))
+
+      img_folder = os.path.join('/home/mlpa/data_4T/experiment_results/ljy/results', case+'_' +str(ep))
       img_folder1 = os.path.join(img_folder, 'real')
       img_folder2 = os.path.join(img_folder, 'fake')
       imgs1 = sorted(glob.glob(os.path.join(img_folder1, "*")))
