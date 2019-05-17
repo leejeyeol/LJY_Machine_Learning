374d5e88c24df03c079c714cbfb3d3173bb53572
DESKTOP-RH6PUJO\rnt <egg5562@naver.com>
2019-05-18 00:57:07 +0900

    exp2 test3

--- PSNR.py
+++ PSNR.py
@@ -47,35 +47,35 @@ if __name__ == '__main__':
             ep = ours_ep[i]
 
 
-    img_folder = os.path.join('/home/mlpa/data_4T/experiment_results/ljy/results', case+'_' +str(ep))
-    img_folder1 = os.path.join(img_folder, 'real')
-    img_folder2 = os.path.join(img_folder, 'fake')
-    imgs1 = sorted(glob.glob(os.path.join(img_folder1, "*")))
-    imgs2 = sorted(glob.glob(os.path.join(img_folder2, "*")))
-    num_of_imgs = len(imgs1) if len(imgs1) <= len(imgs2) else len(imgs2)
+        img_folder = os.path.join('/home/mlpa/data_4T/experiment_results/ljy/results', case+'_' +str(ep))
+        img_folder1 = os.path.join(img_folder, 'real')
+        img_folder2 = os.path.join(img_folder, 'fake')
+        imgs1 = sorted(glob.glob(os.path.join(img_folder1, "*")))
+        imgs2 = sorted(glob.glob(os.path.join(img_folder2, "*")))
+        num_of_imgs = len(imgs1) if len(imgs1) <= len(imgs2) else len(imgs2)
 
-    result_data = []
-    num_of_nan = 0
-    for i in range(num_of_imgs):
-        #print('%s,%s'%(imgs1[i],imgs2[i]))
-        npImg1 = cv2.imread(imgs1[i])
-        npImg2 = cv2.imread(imgs2[i])
-        img1 = np.rollaxis(npImg1, 2).reshape((1,3,64,64))
-        img2 = np.rollaxis(npImg2, 2).reshape((1,3,64,64))
-        PSNR=(psnr(img1, img2))
-        if not math.isnan(PSNR):
-            print('[%d/%d] %f'%(i,num_of_imgs,PSNR))
-            result_data.append(PSNR)
-        else:
-            print('nan')
-            num_of_nan +=1
+        result_data = []
+        num_of_nan = 0
+        for i in range(num_of_imgs):
+            #print('%s,%s'%(imgs1[i],imgs2[i]))
+            npImg1 = cv2.imread(imgs1[i])
+            npImg2 = cv2.imread(imgs2[i])
+            img1 = np.rollaxis(npImg1, 2).reshape((1,3,64,64))
+            img2 = np.rollaxis(npImg2, 2).reshape((1,3,64,64))
+            PSNR=(psnr(img1, img2))
+            if not math.isnan(PSNR):
+                print('[%d/%d] %f'%(i,num_of_imgs,PSNR))
+                result_data.append(PSNR)
+            else:
+                print('nan')
+                num_of_nan +=1
 
-    result_data_ = np.asarray(result_data)
-    result_mean = result_data_.mean()
-    if supplementNaN :
-        for i in range(num_of_nan):
-            result_data.append(result_mean)
+        result_data_ = np.asarray(result_data)
+        result_mean = result_data_.mean()
+        if supplementNaN :
+            for i in range(num_of_nan):
+                result_data.append(result_mean)
 
-    np_PSNR_data = np.asarray(result_data)
-    np.save('/home/mlpa/data_ssd/workspace/experimental_result/LJY/VAEGAN_measure_results/%s_%d.npy'%(case,ep), np_PSNR_data)
-    print('mean is %f' % result_mean)
+        np_PSNR_data = np.asarray(result_data)
+        np.save('/home/mlpa/data_ssd/workspace/experimental_result/LJY/VAEGAN_measure_results/%s_%d.npy'%(case,ep), np_PSNR_data)
+        print('mean is %f' % result_mean)
