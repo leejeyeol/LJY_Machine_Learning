e87dfdcc083d5c83d6c69dbbbed0bdbf84a8b495
DESKTOP-RH6PUJO\rnt <egg5562@naver.com>
2019-06-24 05:55:34 +0900

    exp4

--- Pytorch/experiment.sh
+++ Pytorch/experiment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for i in {0..3}
+for ((i=0;i<=3;i++))
 do
-   python3 /home/mlpa/data_ssd/workspace/github/LJY_Machine_Learning/Pytorch/GAN/VAE+GAN.py --modelOutFolder /home/mlpa/data_4T/experiment_results/model/LJY/VAEGAN_cifar10_dcgan_1 --preset alpha-gan --cuda --dataset CIFAR10 --batchSize 64 --runfunc Generate --pretrainedEpoch $i
-   python3 /home/mlpa/data_ssd/workspace/github/LJY_Machine_Learning/inception_score.py
+    python3 /home/mlpa/data_ssd/workspace/github/LJY_Machine_Learning/Pytorch/GAN/VAE+GAN.py --modelOutFolder /home/mlpa/data_4T/experiment_results/model/LJY/VAEGAN_cifar10_dcgan_1 --preset alpha-gan --cuda --dataset CIFAR10 --batchSize 64 --runfunc Generate --pretrainedEpoch $i
+    python3 /home/mlpa/data_ssd/workspace/github/LJY_Machine_Learning/inception_score.py
 done
 
 
